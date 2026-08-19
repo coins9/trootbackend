1d2f4396765d7003bbde3ca4892c67e740f85ae3
jiyeong <amber9291@naver.com>
2026-08-19 21:44:40 +0900

    z

--- src/modules/reservation/reservation.module.ts
+++ src/modules/reservation/reservation.module.ts
@@ -5,10 +5,11 @@ import { ReservationService } from './application/reservation.service';
 import { Reservation } from './domain/reservation.entity';
 import { User } from '../user/domain/user.entity';
 import { ArtistPage } from '../artist/domain/artist.entity';
+import { Artwork } from '../artist/domain/artwork.entity';
 import { AppReservationController } from './presentation/app-reservation.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reservation, User, ArtistPage]), ArtistModule],
+  imports: [TypeOrmModule.forFeature([Reservation, User, ArtistPage, Artwork]), ArtistModule],
   controllers: [AppReservationController],
   providers: [ReservationService],
   exports: [ReservationService],
